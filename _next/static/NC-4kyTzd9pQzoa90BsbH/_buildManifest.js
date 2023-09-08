f9c1a6ff172ec2b32960d436aa51268f50604eec
feyi-tech <jinminetics@gmail.com>
2023-09-08 09:16:51 +0100

    build

---
+++ _next/static/NC-4kyTzd9pQzoa90BsbH/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/1bfc9850-8195263292953888.js","static/chunks/849-a5923a74c5be413f.js","static/chunks/pages/index-cbde32b3b09fad5a.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
